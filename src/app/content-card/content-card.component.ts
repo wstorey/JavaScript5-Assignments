3f7c469648feb20ffbbd3e50db63673259845b4b
Will Storey <will@willstorey.ca>
2020-01-16 18:56:46 -0500

    Re-did ContentList class methods

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -12,20 +12,22 @@ export interface Content {
 }
 
 class ContentList {
-  static contentCount;
+  // set types(type: Content[]) {
+  //   this._types = type;
+  // }
+  // static htmlContent(i: number) {
+  //   // return ContentList[i];
+  //   return this.htmlContent(ContentList[i]);
+  // }
   private _types: Content[];
   constructor() {
     this._types = [];
-    ContentList.increaseCount();
   }
   get types(): Content[] {
     return this._types;
   }
-  set types(i: Content[]) {
-    this._types = i;
-  }
-  static increaseCount() {
-    return ContentList.contentCount++;
+  static addContentToArray(content: Content, _types: Content[]) {
+    this._types.push(content);
   }
 }
 
@@ -35,10 +37,24 @@ class ContentList {
   styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent implements OnInit {
-
-  constructor() { }
+  content: Content;
+  cList: ContentList;
+  constructor() {
+    this.content = {
+      id: 1,
+      author: 'Will',
+      imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+      type: 'tech',
+      title: 'This Is The Title',
+      body: 'This is the body content',
+      tags: ['tech', 'Will']
+    };
+    this.cList = new ContentList();
+    // this.cList.types(this.content);
+  }
 
   ngOnInit() {
+
   }
 
 }
