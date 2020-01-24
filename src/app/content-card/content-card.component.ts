7316efa7557ea130404bb7f365a48792c04d5206
Will Storey <will@willstorey.ca>
2020-01-24 16:08:50 -0500

    Finished Assign2 with styling and proper use of multiple components

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {Content} from './content-card-helper';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
 
+  @Input() content: Content;
   constructor() {
 
   }
 
   ngOnInit() {
 
+  }
+  displayId(item) {
+    console.log(`ID of ${item.title} id: ${item.id}`);
   }
 }
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -4,70 +4,69 @@ import { Content } from '../content-card/content-card-helper';
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.css']
+  styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
   content: Content[];
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
     this.content = [
       {
         id: 1,
         author: 'Will',
-        imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-        type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        imgUrl: 'https://i.ytimg.com/vi/kkg9BhISfdA/maxresdefault.jpg',
+        type: 'fiction',
+        title: 'Non-Video Video Essay on Anime',
+        body: 'Looking at my favourite shows in a word-to-eye format.',
+        tags: ['fiction', 'Will']
       },
       {
         id: 2,
-        author: 'Will',
-        imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-        type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        author: 'Luca',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Brindis_%2824675281395%29.jpg',
+        type: 'fiction',
+        title: 'Water',
+        body: 'A fluid novel',
+        // tags: ['fiction', 'Luca']
       },
       {
         id: 3,
-        author: 'Will',
-        imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+        author: 'Draven',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Shades_of_light_blue.png',
         type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        title: 'The Sky is Blue',
+        body: 'Why tho?',
+        tags: ['environmental science', 'Draven']
       },
       {
         id: 4,
-        author: 'Will',
-        imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-        type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        author: 'Jack',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/79/Competitive_table_tennis.jpg',
+        type: 'Table',
+        title: 'Table',
+        body: 'The true story',
+        tags: ['table?', 'Jack']
       },
       {
         id: 5,
-        author: 'Will',
-        imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+        author: 'Nathan',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Hello_World_Brian_Kernighan_1978.jpg',
         type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        title: 'Hello World',
+        body: 'My first program - psychological look into Hello World',
+        tags: ['tech', 'Nathan']
       },
       {
         id: 6,
         author: 'Will',
         imgUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
         type: 'tech',
-        title: 'This Is The Title',
-        body: 'This is the body content',
-        tags: ['tech', 'Will']
+        title: 'This Is should not show up',
+        body: 'Look away',
+        tags: ['Nah']
       }
     ];
   }
-
 }
