23fb95bd40510e9f7799b0a14b4f201b9ff8f171
Will Storey <will@willstorey.ca>
2020-01-30 22:53:32 -0500

    Issues with contentFilter pipe

--- src/app/content-filter.pipe.ts
+++ src/app/content-filter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './content-card/content-card-helper';
 
 @Pipe({
   name: 'contentFilter'
 })
 export class ContentFilterPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(book: Content): string {
+    return book.type || 'fiction';
   }
+
+  // transform(contentList: Content[], allowNullTags?: boolean): Content[] {
+  //   return contentList.filter( (c) => c.tags != null ? c.tags.length !== 0 : null);
+  // }
 
 }
